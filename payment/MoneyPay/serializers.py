60bdf002937be2ff3abf044c7a39b0598dd6d1d3
Manish Kumar <manis.kumar@gmail.com>
2022-12-17 14:31:37 +0530

    Fixed user model issue

--- payment/MoneyPay/serializers.py
+++ payment/MoneyPay/serializers.py
@@ -1,11 +1,14 @@
-from django.contrib.auth.models import User, Group
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group
 from rest_framework import serializers
+
+User = get_user_model()
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = ['username']
+        fields = ['username', 'email', 'phone_number']
 
 
 class GroupSerializer(serializers.HyperlinkedModelSerializer):
